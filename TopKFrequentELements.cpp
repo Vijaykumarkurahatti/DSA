ed2264cb8f276e5c611b9af8815a3bfaa6711624
Vijaykumarkurahatti <55583877+Vijaykumarkurahatti@users.noreply.github.com>
2024-11-25 21:03:52 +0530

    Create TopKFrequentELements.cpp

---
+++ TopKFrequentELements.cpp
@@ -0,0 +1,22 @@
+/*
+- take n as numbers size
+- take unorder map m
+- for loop on i 0 to n
+       - map of number [i] ++
+- take vectot vector int buckets n +1
+
+for loop on map
+    - bucket[it -> 2] push it firsy
+
+- take vector result
+for loop on i =n to > = to 0 i--
+   - if result size >= k
+       breal for loop
+   - if not bucket of i empty 
+        - result insert (result end , bucket of i begin, bucket of i end)
+
+  last return resukt
+  
+
+
+*/
