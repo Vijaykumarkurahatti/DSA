43226b94109c1ad630e1c78261b008003dca1a5f
Vijaykumarkurahatti <55583877+Vijaykumarkurahatti@users.noreply.github.com>
2024-11-25 21:32:11 +0530

    Create contain Duplicates.cpp

---
+++ contain Duplicates.cpp
@@ -0,0 +1,7 @@
+- take un set
+- for loop on i: 0 to num size
+     if s et find nums of i not = set end
+         then return true  (contan duolicaees)
+     set insert number of i
+
+return false( do not contain duplicates)
