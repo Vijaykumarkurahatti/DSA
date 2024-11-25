5c3fb357d27c0cf6d40b97df9c7a2ffc264f4e66
Vijaykumarkurahatti <55583877+Vijaykumarkurahatti@users.noreply.github.com>
2024-11-25 20:53:45 +0530

    Create arrayOfProductExceptSelf.cpp

---
+++ arrayOfProductExceptSelf.cpp
@@ -0,0 +1,19 @@
+/*
+--    take n nums size
+--    take result vector int  with n size and all 1
+--     take prefix as 1
+--     for loop i -0 to n
+        - assign each result [i] to prefix
+        - prefix to prefix x  each nuber[i]
+        
+       - tske postfix=1
+       for loop on ( from n-1 to i > = 0 i--)
+       - assign res[i] to reslt[i] * postfix
+       - psotfix = postfix x number of i
+
+       return the result
+       
+        
+
+
+*/
