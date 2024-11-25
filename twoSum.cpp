ce0803ed7b1e55029b4d13b1709f355358189b50
Vijaykumarkurahatti <55583877+Vijaykumarkurahatti@users.noreply.github.com>
2024-11-25 21:21:57 +0530

    Create twoSum.cpp

---
+++ twoSum.cpp
@@ -0,0 +1,15 @@
+/*
+- take n num size
+- take un map m
+- for loop  i: 0 to n 
+      - take compliment is target - nums of i
+      - if map of find num of i  not equal to map end
+              return {mp of comp,i}
+      - map insert( {nums of i, i})
+
+return {}
+
+      
+
+
+*/
