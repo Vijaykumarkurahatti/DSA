4c4bf5e2fae8452ffbefc25df650b1d5ceaf1604
Vijaykumarkurahatti <55583877+Vijaykumarkurahatti@users.noreply.github.com>
2024-11-25 21:28:32 +0530

    Create isAnagram.cpp

---
+++ isAnagram.cpp
@@ -0,0 +1,13 @@
+-   check s size not equal t size then return false (Not Anagram)
+  - take un map smap
+  - take un map tmap
+- for loop on s size
+   -  smap of s of i ++
+   - tmap of t of i ++
+
+- for loop on key value of smap
+  - if value  not = key of tmap
+       then ret false   (not angram)
+
+  retue true (is anagram) at last
+  
