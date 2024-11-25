8a00e8eb107b7f846f5f308d2c55e49cb25ba861
Vijaykumarkurahatti <55583877+Vijaykumarkurahatti@users.noreply.github.com>
2024-11-25 20:45:32 +0530

    Create EncodeandDecode.cpp

---
+++ EncodeandDecode.cpp
@@ -0,0 +1,22 @@
+/*
+Encode
+- Take result string
+- for loop on strings
+      take one string
+      result +=  to dtring(one str size) + # + one sstr;
+   return result
+
+Decode
+- vector result(string)
+while on i until string size
+     - j is i
+     while until s[j] not = #
+       -j++
+    - tak lenth  str to int convert (sub string  (i , j-i)
+    - str = sub string j+1, length
+    - result push str
+    - icreement i to  i +1 +length
+
+return result
+
+*/
