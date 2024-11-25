51aacbaf8f2ba1d81fa3e53e8bcbd806d76c0798
Vijaykumarkurahatti <55583877+Vijaykumarkurahatti@users.noreply.github.com>
2024-11-25 20:33:23 +0530

    Create Longest Consecutive sequence.cpp

---
+++ Longest Consecutive sequence.cpp
@@ -0,0 +1,11 @@
+/*
+-Take one set ( add all nums)
+-take longest a zero initially
+-for(s)
+  -if( not s count(n-1)  take length 1 and while loop on  scount of n + lemgth  ++ length  out of if long=max(long,length)
+
+  - return long
+    
+
+
+*/
