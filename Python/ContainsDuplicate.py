ce3a90b21b4fd25f51373b441d50fbf53b6a31d3
Vijaykumarkurahatti <55583877+Vijaykumarkurahatti@users.noreply.github.com>
2024-12-27 15:22:08 +0530

    Update ContainsDuplicate.py

--- Python/ContainsDuplicate.py
+++ Python/ContainsDuplicate.py
@@ -1,1 +1,8 @@
-class Solution(object)
+class Solution(object):
+  def containsDuplicate(self,nums):
+    hashSet = set()
+    for n in nums:
+      if n in hashSet:
+        return True
+      hashSet.add(n)
+    return False
